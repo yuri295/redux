06003275bf6ce9c67ca08fe64dbb22ac2b31f39a
yuri295 <jin_astro0302@naver.com>
2024-02-01 22:14:50 +0900

    feat: combine reducers

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -3,12 +3,16 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import './App.css';
 import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
+import IceCreamContainer from './components/IceCreamContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <HooksCakeContainer></HooksCakeContainer>
         <CakeContainer />
+        <IceCreamContainer />
       </div>
     </Provider>
   );
